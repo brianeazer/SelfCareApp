a7dc914d992b465722f1b8dfe17e2ee4aa5837e9
Mariah Hall <hallmar94@gmail.com>
2018-11-29 16:01:40 -0500

    initial commit of final project- self care app

---
+++ src/main/java/co/grandcircus/selfcareapp/MainController.java
@@ -0,0 +1,10 @@
+package co.grandcircus.selfcareapp;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+	
+	
+}
---
+++ src/main/java/co/grandcircus/selfcareapp/SelfCareAppApplication.java
@@ -0,0 +1,12 @@
+package co.grandcircus.selfcareapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfCareAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfCareAppApplication.class, args);
+	}
+}
