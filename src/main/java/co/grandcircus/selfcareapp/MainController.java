b76b77d55d32a5c5d873caf3f5d3ee7a5bef2d5f
Michael Kincaid <mbkincaid.1@gmail.com>
2018-12-05 11:07:49 -0500

    deleted merge conflict from jsp

--- src/main/java/co/grandcircus/selfcareapp/MainController.java
+++ src/main/java/co/grandcircus/selfcareapp/MainController.java
@@ -2,11 +2,8 @@ package co.grandcircus.selfcareapp;
 
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-<<<<<<< Updated upstream
 import java.util.Arrays;
-=======
 import java.util.Collections;
->>>>>>> Stashed changes
 import java.util.List;
 
 import javax.servlet.http.Cookie;
@@ -175,24 +172,25 @@ public class MainController {
 		ModelAndView mv = new ModelAndView("top10likes");
 		User user = (User) session.getAttribute("user");
 		System.out.println(user.getUsername());
-		ArrayList<UserLikes> likes = (ArrayList<UserLikes>) likeDao.getUserLikes(user);
 		
+		ArrayList<UserLikes> likes = (ArrayList<UserLikes>) likeDao.getUserLikes(user);
+		ArrayList<UserLikes> top10 = getTop10(likes);
+		
+		mv.addObject("likes", top10);
+		return mv;
+	}
+	public ArrayList <UserLikes> getTop10(ArrayList<UserLikes> likes) {
 		Collections.sort(likes, (l1, l2) -> l1.getCount().compareTo(l2.getCount()));
-	
+		
 		ArrayList <UserLikes> top10 = new ArrayList<>();
 		
 		for (int i = likes.size() - 1; i > likes.size() - 11; i--) {
 			
 			top10.add(likes.get(i));
-			
 		}
-		
 		for (UserLikes ul : top10) {
 			System.out.println(ul.getTag() + ul.getCount());
-			
 		}
-		mv.addObject("likes", top10);
-		return mv;
+		return top10;
 	}
-
 }
