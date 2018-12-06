baaa1a53f724b3862f9738c3abeb47f6889b7dad
Michael Kincaid <mbkincaid.1@gmail.com>
2018-12-06 15:33:50 -0500

    added error jsp and added new method to controller

--- src/main/java/co/grandcircus/selfcareapp/MainController.java
+++ src/main/java/co/grandcircus/selfcareapp/MainController.java
@@ -1,5 +1,6 @@
 package co.grandcircus.selfcareapp;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -11,6 +12,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -301,5 +307,42 @@ public class MainController {
 		return num;
 	}
 
+	@WebServlet("/ErrorHandler")
+	public class ErrorHandler extends HttpServlet {
+	    private static final long serialVersionUID = 1L;
+
+	    protected void doGet(HttpServletRequest request,
+	            HttpServletResponse response) throws ServletException, IOException {
+	        processError(request, response);
+	    }
+
+	    protected void doPost(HttpServletRequest request,
+	            HttpServletResponse response) throws ServletException, IOException {
+	        processError(request, response);
+	    }
+	    private void processError(HttpServletRequest request,
+	            HttpServletResponse response) throws IOException, ServletException {
+	        //customize error message
+	        Throwable throwable = (Throwable) request
+	                .getAttribute("javax.servlet.error.exception");
+//	        Integer statusCode = (Integer) request
+//	                .getAttribute("javax.servlet.error.status_code");
+	        String servletName = (String) request
+	                .getAttribute("javax.servlet.error.servlet_name");
+	        if (servletName == null) {
+	            servletName = "Unknown";
+	        }
+	        String requestUri = (String) request
+	                .getAttribute("javax.servlet.error.request_uri");
+	        if (requestUri == null) {
+	            requestUri = "Unknown";
+	        }    
+	        request.setAttribute("error", "Servlet " + servletName + 
+	          " has thrown an exception " + throwable.getClass().getName() +
+	          " : " + throwable.getMessage());    
+	        request.getRequestDispatcher("/error.jsp").forward(request, response);
+	    }
+
+	}
 	
 }
