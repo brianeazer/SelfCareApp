1e9c03dd3be8c0932f5749a6587f6da22bb43cc2
Michael Kincaid <mbkincaid.1@gmail.com>
2018-12-10 15:00:31 -0500

    push

--- src/main/java/co/grandcircus/selfcareapp/Entity/UserDate.java
+++ src/main/java/co/grandcircus/selfcareapp/Entity/UserDate.java
@@ -17,6 +17,40 @@ public class UserDate {
 	@ManyToOne
 	private User user;
 	private Date date;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public UserDate(Long id, User user, Date date) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.date = date;
+	}
+	public UserDate() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "UserDate [id=" + id + ", user=" + user + ", date=" + date + "]";
+	}
+	
+	
+	
 	
 	
 	
